66117718c916f3fb6e4a6a2ae9bae43c9eb0f54d
seichi <seichi8901@gmail.com>
2023-06-18 18:37:25 +0900

    マウスのx座標と一番近いプロットのy座標をマウスのy座標と同じにするように変更

--- sin_wave_editting.py
+++ sin_wave_editting.py
@@ -1,68 +1,71 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy.random import rand
 
 from util.view_controller import ViewController
-# クリックした自転でのマウスカーソルの位置から半径maxdいないのプロット点の座標を取得する
-# propsにはeventの属性を定義する辞書を指定する
-# この場合はind, pickx, pickyという属性にデータ番号、データx座標、データy座標を指定している
-
 
 class PlotPicker:
 
     def __init__(self, fig, ax):
         self.fig = fig
         self.ax = ax
-        self.idxs = None
+        self.before_mouse_xdata = None
+        self.before_mouse_ydata = None
 
         for line in self.ax.lines:
-            line.set_picker(self.line_picker)
-        fig.canvas.mpl_connect('pick_event', self.onpick2)
+            line.set_picker(self.dummy_picker)
         fig.canvas.mpl_connect('motion_notify_event', self.motion)
 
     def motion(self, event):
-        if event.button == 1 and self.idxs:
+        if event.xdata is None:
+            self.before_mouse_xdata = None
+            self.before_mouse_ydata = None
+            return
+        if event.button == 1:
+            xdata = self.ax.lines[0].get_xdata()
             ydata = self.ax.lines[0].get_ydata()
-            ydata[self.idxs[0]] = event.ydata
+            
+            idx = np.argmin(np.abs(xdata - event.xdata))
+            
+            # 線形補間
+            if self.before_mouse_xdata is not None:
+                b_idx = np.argmin(np.abs(xdata - self.before_mouse_xdata))
+                n_step = abs(idx - b_idx)
+                
+                if n_step != 0:
+                    diff = event.ydata -self.before_mouse_ydata
+                    step_d = diff/n_step
+                    
+                    if b_idx < idx:
+                        for n, idx in enumerate(range(b_idx,idx+1)):
+                            ydata[idx] = self.before_mouse_ydata + step_d*n
+                    else:
+                        for n, idx in enumerate(reversed(range(idx,b_idx+1))):
+                            ydata[idx] = self.before_mouse_ydata + step_d*n
+            else:
+                ydata[idx] = event.ydata
+                
             self.ax.lines[0].set_ydata(ydata)
-        # gco.set_data(x, y)
-        plt.draw()
+            
+        self.before_mouse_xdata = event.xdata
+        self.before_mouse_ydata = event.ydata
+        self.fig.canvas.draw()
 
-    def onpick2(self, event):
-        print('onpick2 line:', event.pickx, event.picky)
-        self.idxs = event.ind
-
-    def line_picker(self, artist, mouseevent):
+    def dummy_picker(self, artist, mouseevent):
         """
-        Find the points within a certain distance from the mouseclick in
-        data coords and attach some extra attributes, pickx and picky
-        which are the data points that were picked.
+        何もしないダミー
         """
-        if mouseevent.xdata is None:
-            return False, dict()
-        xdata = artist.get_xdata()
-        ydata = artist.get_ydata()
-        maxd = 0.05
-        d = np.sqrt(
-            (xdata - mouseevent.xdata)**2 + (ydata - mouseevent.ydata)**2)
-
-        ind, = np.nonzero(d <= maxd)
-        if len(ind):
-            pickx = xdata[ind]
-            picky = ydata[ind]
-            props = dict(ind=ind, pickx=pickx, picky=picky)
-            return True, props
-        else:
-            return False, dict()
+        return False, dict()
 
 
 fig, ax = plt.subplots()
 ax.set_title('custom picker for line data')
-x = np.arange(2*np.pi*10)/10
+x = np.arange(2*np.pi*5)/5
 y = np.sin(x)
 
-ax.plot(x, y, marker='o')
+ax.plot(x, y, marker='.')
 vc = ViewController(fig, ax)
 pp = PlotPicker(fig, ax)
 
+print(ax.lines[0].get_ydata())
 plt.show()
+print(ax.lines[0].get_ydata())
